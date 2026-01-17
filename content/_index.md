34a97f0cb259acb59808548d004d000ac4d12584
mcnair <mcnair@riseup.net>
2026-01-17 11:59:56 -0500

    Initial Hugo site

---
+++ content/_index.md
@@ -0,0 +1,4 @@
+---
+title: "CoopVPN - A Worker-Owned VPN"
+description: "Join the waitlist to get early access to a worker-owned alternative to corporate VPNs."
+---
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    './layouts/**/*.html',
+    './content/**/*.{html,md}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'purple': '#301731',
+        'cream': '#FDF6F1',
+        'accent': '#f4c430',
+      },
+    },
+  },
+  plugins: [],
+}
